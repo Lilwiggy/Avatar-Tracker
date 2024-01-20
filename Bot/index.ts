c99c1970635775852bc340a5133a01ec0eb12717
Belle Wiggins <lilwiggy123@gmail.com>
2024-01-19 22:18:53 -0800

    Fix major personal bug
    Fixed a bug where bot would crash in my personal use cases where it would send a message in a server of mine. This bug does not effect anyone else.

--- Bot/index.ts
+++ Bot/index.ts
@@ -1,8 +1,8 @@
 import { fetchUser } from './utils';
-import { CommandClient, GatewayClientEvents } from 'detritus-client';
+import { CommandClient, GatewayClientEvents, ShardClient } from 'detritus-client';
 import { parse } from "./parseIconData.js";
-import { ChannelGuildText } from 'detritus-client/lib/structures';
-import { ClientEvents } from 'detritus-client/lib/constants';
+import { ChannelGuildText, ChannelGuildVoice } from 'detritus-client/lib/structures';
+import { ActivityTypes, ClientEvents } from 'detritus-client/lib/constants';
 import pgPromise from 'pg-promise';
 import config from './config';
 
@@ -28,9 +28,8 @@ let gender = 'female';
 
     // These are used for personal use. Please ignore if you fork this bot and make it for yourself
     await client.rest.fetchGuild('1079483049445687418');
-    let tracker: ChannelGuildText = await client.rest.fetchChannel('1079483050628501557');
-    let count: ChannelGuildText = await client.rest.fetchChannel('1092514036605779988');
-    let genChat: ChannelGuildText = await client.rest.fetchChannel('1079483050628501556');
+    let tracker: ChannelGuildVoice = await client.rest.fetchChannel('1079483050628501557');
+    let count: ChannelGuildVoice = await client.rest.fetchChannel('1092514036605779988');
     let name: string = tracker.name.split(': ')[1];
     let n: number = parseInt(count.name.split(': ')[1]);
     gender = name;
@@ -45,21 +44,21 @@ let gender = 'female';
             await parse(db, differences.user); // Parse and store data about the avatar changed
 
             if (differences.user.id === '282018830992277504') {
-                genChat.createMessage('Hey @everyone! Lexi just changed they icon. Rolling for a new gender...');
+                client.rest.createMessage('1079483050628501556', 'Hey @everyone! Lexi just changed they icon. Rolling for a new gender...');
 
                 let coin = Math.floor(Math.random() * 2);
                 if (coin === 1) gender = 'male';
                 else gender = 'female';
-                tracker.edit({
+                client.rest.editChannel('1079483050628501557', {
                     name: `Lexi is: ${gender}`,
                 });
 
-                genChat.createMessage(`Coin: Flipped. Landed on ${coin === 1 ? 'Heads' : 'Tails'} which means that Lexi is now a ${coin === 1 ? 'Male' : 'Female'}.`);
+                client.rest.createMessage('1079483050628501556',`Coin: Flipped. Landed on ${coin === 1 ? 'Heads' : 'Tails'} which means that Lexi is now a ${coin === 1 ? 'Male' : 'Female'}.`);
             } else if (differences.user.id === '232614905533038593') {
-                count.edit({
-                    name: `Belle changes: ${++n}`
+                client.rest.editChannel('1092514036605779988', {
+                    name: `Belle changes: ${++n}`,
                 });
-                genChat.createMessage(`Hey so fuckin, Belle changed her icon AGAIN... This makes ${n} times since I have been watching...`);
+                client.rest.createMessage('1092514036605779988', `Hey so fuckin, Belle changed her icon AGAIN... This makes ${n} times since I have been watching...`)
             }
         }
     });
