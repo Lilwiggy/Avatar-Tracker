81e7bc5a52acd3dbaba0195bab6ae3ec968acaa2
Belle Wiggins <lilwiggy123@gmail.com>
2024-06-19 22:13:41 -0700

    Fixed major bugs + cleaned up code
    I don't even know where to begin. I was high when coding this and I still think that I was off my rockers when I wrote this originally.

--- Bot/index.ts
+++ Bot/index.ts
@@ -1,14 +1,11 @@
-import { addAvatar, fetchUser } from './utils';
-import { CommandClient, GatewayClientEvents, ShardClient, InteractionCommandClient } from 'detritus-client';
-import { parse } from "./parseIconData.js";
+import { RETURN_CODES, addAvatar, fetchUser } from './utils';
+import { CommandClient, GatewayClientEvents, InteractionCommandClient } from 'detritus-client';
 import { ChannelGuildVoice } from 'detritus-client/lib/structures';
-import { ActivityTypes, ClientEvents, InteractionCallbackTypes } from 'detritus-client/lib/constants';
+import { ClientEvents, InteractionCallbackTypes } from 'detritus-client/lib/constants';
 import pgPromise from 'pg-promise';
 import config from './config';
-import { fetchColor } from './fetchAverageColor';
 
 const pgp = pgPromise();
-
 
 const cmdClient = new CommandClient(config.token, {
     gateway: {
@@ -31,11 +28,15 @@ let gender = 'female';
             } else {
                 await db.query('INSERT INTO users (id, track) VALUES ($1, true)', context.userId);
                 context.user.track = true;
-                await parse(db, context.user);
-                return context.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, 'Thank you for subscribing! Your current avatar has been added to the database and I will add your new ones automatically!');
+                let res = await addAvatar(db, context.user);
+                if (res === RETURN_CODES.IMGUR_FAILED)
+                    return context.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, 'I apologize, an issue occured with imgur\'s API. You are still set to be tracked in the future but your avatar did not upload to our database. Please try the /check command in a few minutes to retry uploading your avatar.');
+                else
+                    return context.respond(InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE, 'Thank you for subscribing! Your current avatar has been added to the database and I will add your new ones automatically!');
             }
         },
       });
+
     const client = await cmdClient.run();
     await interactionClient.run();
     await interactionClient.uploadApplicationCommands().catch(console.error);
@@ -60,7 +61,7 @@ let gender = 'female';
         if (differences && differences.differences.avatar) {
             let user = await fetchUser(db, differences.user.id);
             if (!user || !user.track) return; // I do be respecting people's privacy
-            await parse(db, differences.user); // Parse and store data about the avatar changed
+            await addAvatar(db, differences.user); // Parse and store data about the avatar changed
 
             if (differences.user.id === '282018830992277504') {
                 client.rest.createMessage('1079483050628501556', 'Hey @everyone! Lexi just changed they icon. Rolling for a new gender...');
@@ -86,6 +87,15 @@ let gender = 'female';
         let msg = paylaod.message;
         if (msg.content.toLowerCase() === '!test') {
             msg.channel.createMessage('yo');
+        }
+
+
+        if (msg.content.toLocaleLowerCase() === '!check') {
+            let res = await addAvatar(db, msg.author);
+            if (res === RETURN_CODES.ALREADY_IN_DB)
+                msg.channel.createMessage('This avatar is already logged!');
+            else
+                msg.channel.createMessage('The avatar was not logged... but it is now!') 
         }
     });
 })();
--- Bot/utils.ts
+++ Bot/utils.ts
@@ -1,4 +1,7 @@
 import { IDatabase } from "pg-promise";
+import config from "./config";
+import { User } from "detritus-client/lib/structures";
+import { fetchColor } from "./fetchAverageColor";
 
 declare module 'detritus-client/lib/structures/user' {
     interface User {
@@ -7,26 +10,15 @@ declare module 'detritus-client/lib/structures/user' {
     }
   }
 
-async function fetchUser(db: IDatabase<{}>, id: string) {
-        let user = await db.oneOrNone('SELECT * FROM users WHERE id = $1', id); // If User is not in DB do not add, this function will be called on all UserUpdates and I do not want to save everyone the bot sees
-        return user;
-};
+// ty stackoverflow
+function rgbToHex(r, g, b) {
+    return "#" + (1 << 24 | r << 16 | g << 8 | b).toString(16).slice(1);
+}
 
-async function addAvatar(db: IDatabase<{}>, icon: Icon, id: string) {
-    let hash: string = '';
-    // Sometimes icon will be null if user is not cached.
-    if (icon.hash === null || icon.hash === 'null')
-        hash = icon.link.split(id + '/')[1].split('.')[0]; // Grab hash manually from discord's CDN eg. https://cdn.discordapp.com/avatars/USER_ID/HASH.png
-    else
-        hash = icon.hash;
-
-    // Check if avatar is in DB
-    let d = await db.oneOrNone('SELECT * FROM user_data WHERE user_id = $1 AND hash = $2', [id, hash]);
-
-    if (d) return;
-
-    await db.query("INSERT INTO user_data (user_id, url, hash, time_set, average_color) VALUES ($1, $2, $3, $4, $5);", [id, icon.link, hash, icon.date, icon.averageColor]);
-    console.log('[DATABASE] Saved avatar', icon);
+let RETURN_CODES = {
+    ALREADY_IN_DB: 0,
+    DATABASE_SUCCEED: 1,
+    IMGUR_FAILED: 2,
 }
 
 interface Icon {
@@ -36,9 +28,53 @@ interface Icon {
     link?: string;
 }
 
-interface Icons {
-    [id: string]: Array<Icon>
+async function fetchUser(db: IDatabase<{}>, id: string) {
+        let user = await db.oneOrNone('SELECT * FROM users WHERE id = $1', id); // If User is not in DB do not add, this function will be called on all UserUpdates and I do not want to save everyone the bot sees
+        return user;
 };
 
+async function addAvatar(db: IDatabase<{}>, user: User): Promise<number> {
+    console.log(config.imgur.clientID)
+    let hash: string = '';
+    let today = new Date();
+    let rgb = await fetchColor(user.avatarUrl);
+    let hex = rgbToHex(rgb[0], rgb[1], rgb[2]);
+    // Sometimes icon will be null if user is not cached.
+    if (user.avatar === null || user.avatar === 'null')
+        hash = user.avatarUrl.split(user.id + '/')[1].split('.')[0]; // Grab hash manually from discord's CDN eg. https://cdn.discordapp.com/avatars/USER_ID/HASH.png
+    else
+        hash = user.avatar;
 
-export { Icon, Icons, fetchUser, addAvatar };
+            // Check if avatar is in DB
+    let d = await db.oneOrNone('SELECT * FROM user_data WHERE user_id = $1 AND hash = $2', [user.id, hash]);
+
+    if (d) return RETURN_CODES.ALREADY_IN_DB;
+
+    let image = await fetch(`https://d.lu.je/avatar/${user.id}?size=512`);
+    let r = await fetch('https://api.imgur.com/3/image', {
+        method: 'POST',
+        body: await image.blob(),
+        headers: {
+            'Authorization': `Client-ID ${config.imgur.clientID}`,
+        },
+        redirect: 'follow'
+    }).then(response => response.json());
+
+    if (r.status === 200) {
+            let icon: Icon = {
+                averageColor: hex,
+                date: today.toISOString(),
+                hash: user.avatar,
+                link: r.data.link
+            }
+            await db.query("INSERT INTO user_data (user_id, url, hash, time_set, average_color) VALUES ($1, $2, $3, $4, $5);", [user.id, icon.link, hash, icon.date, icon.averageColor]);
+            console.log('[DATABASE] Saved avatar', icon);
+            return RETURN_CODES.DATABASE_SUCCEED;
+        } else {
+            user.client.rest.createMessage('1253207583120949331', `New Error: \`\`\`json\n ${JSON.stringify(r)} \`\`\` `);
+            return RETURN_CODES.IMGUR_FAILED;
+        }
+}
+
+
+export { fetchUser, addAvatar, RETURN_CODES };
